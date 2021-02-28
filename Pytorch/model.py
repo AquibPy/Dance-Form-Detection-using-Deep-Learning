8dce87a98a73f935dd0a5b66943473e4e01ed080
AquibPy <mohdaquib31@gmail.com>
2021-02-28 22:39:48 +0530

    Fine-tune the model

--- Pytorch/model.py
+++ Pytorch/model.py
@@ -23,7 +23,14 @@ train_set,test_set = torch.utils.data.random_split(dataset,[292,72])
 train_loader = DataLoader(train_set,batch_size=batch_size,shuffle=True)
 test_loader = DataLoader(test_set,batch_size=batch_size,shuffle=True)
 
-model = torchvision.models.densenet121(pretrained=True)
+model = torchvision.models.vgg16_bn(pretrained=True)
+# print(model)
+for param in model.parameters():
+    param.requires_grad= False
+
+model.classifier[6] = nn.Sequential(nn.Linear(4096,8))
+
+# print(model.parameters)
 model.to(device)
 
 criterion = nn.CrossEntropyLoss()
@@ -40,4 +47,19 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.zero_grad()
         optimizer.step()
-    print(f"Cost at Epoch {epoch} is {sum(losses)/len(losses)}")
+    print(f"Loss at Epoch {epoch} is {loss}")
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for images,labels in test_loader:
+        # images, labels = data
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the test images: %d %%' % (
+    100 * correct / total))
